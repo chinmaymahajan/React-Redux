d0ffe6b1ddd63c9c188e0c8b832d8cd1020765df
Chinmay <chimahaj.cisco.com>
2017-06-28 15:56:32 -0400

    added props

--- App/index.js
+++ App/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const Hello = () => {
+const headerTitle = "Header";
+const footerTitle = "Footer";
+
+const Hello = ({ type2, type1 }) => {
   return (
     <div>
-      <h1>Welcome to React-Redux</h1>
+
+			<Header title={type1}/>
+      <h1>Welcome to React-Redux{type2} </h1>
+			<Footer title={type2}/>
     </div>
   )
 }
 
 render(
-	<Hello/>,
+	<Hello type1={headerTitle} type2={footerTitle}/>,
   document.getElementById('root')
 )
+
+Hello.propTypes = {
+	type1: React.PropTypes.string,
+	type2: React.PropTypes.string,
+}
+
+Hello.defaultProps = {
+	title1: 'unknown title'
+}
 
 export default Hello
